9b716367cf91fa34833b51838f77b50c9b2ae5a4
yooaknow <dbdkfma1228@naver.com>
2026-01-02 00:21:41 +0900

    , ;으로 나눠지는 것 확인

--- src/Phase.js
+++ src/Phase.js
@@ -1,5 +1,14 @@
 import { Console } from "@woowacourse/mission-utils";
 
 export function Phase(trim_input){
-  return 0; //문자열이 들어와지고 있는 상태
+  
+  // 숫자 들어와지고 있음. 
+  // 이를 통해 . ;로 나눠지는 로직과, 
+  // // \n 안에 있는 것으로 나눠지게 하는 로직 구현 해야함 "
+
+  let arrayOfString = trim_input.split(/,:/);
+  Console.print(`나눠진 값들: ${arrayOfString}`);
+
+  return 0; 
+
 }
