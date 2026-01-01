1266b2cdef20a9040b04dbd5f64e4d03f77e0b37
yooaknow <dbdkfma1228@naver.com>
2026-01-01 23:40:25 +0900

    feat: imput 값 add.js 넘겨줌

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
-class App {
-  async run() {zz}
+import { Console } from "@woowacourse/mission-utils";
+import { Add } from "./src/Add.js"
+
+class App   {
+  async run() {
+    const input = await Console.readLineAsync(`숫자를 입력해 주세요: `);
+    Console.print(`결과 : ${input}`);
+
+    Add (input);
+
+
+  }
 }
 
 export default App;
---
+++ src/add.js
@@ -0,0 +1,6 @@
+import { Console } from "@woowacourse/mission-utils";
+
+export add (input){
+  Console.print(`결과: ${input}`)
+  
+}
