b0427d655b59c311bccd40619d758b65a23b3654
yooaknow <dbdkfma1228@naver.com>
2026-01-02 00:36:41 +0900

    / /n 사이에 들어간 값 찾기

--- src/Phase.js
+++ src/Phase.js
@@ -6,9 +6,20 @@ export function Phase(trim_input){
   // 이를 통해 . ;로 나눠지는 로직과, 
   // // \n 안에 있는 것으로 나눠지게 하는 로직 구현 해야함 "
 
-  let arrayOfString = trim_input.split(/,:/);
-  Console.print(`나눠진 값들: ${arrayOfString}`);
+  if (trim_input.startsWith("//")){
+    let start = trim_input.indexOf("//") +2;
+    let end = trim_input.indexOf("\\n");
 
-  return 0; 
+    let arrayOfString =  trim_input.substring(start, end);
+    Console.print(`start: ${start}`);
+    Console.print(`// \n 사이에 들어간 값: ${arrayOfString}`);
+
+  } else{
+    let arrayOfString = trim_input.split(/,:/);
+    Console.print(`, ;으로 나눠진 값들: ${arrayOfString}`);
+  }
+
+
+  return; 
 
 }
