53b986f10550861ddb6e1d8f6b08fa5f5213320d
yooaknow <dbdkfma1228@naver.com>
2026-01-01 23:19:42 +0900

    처음부터 구현

--- src/App.js
+++ src/App.js
@@ -1,15 +1,5 @@
-import { Console } from '@woowacourse/mission-utils';
-import { add } from './add.js';
-
 class App {
-  async run() {
-    Console.print("덧셈할 문자열을 입력해 주세요.");
-    const input = await Console.readLineAsync("");
-    const finalResult = add(input);
-
-    Console.print("결과 : " + finalResult);
-
-  }
+  async run() {zz}
 }
 
 export default App;
