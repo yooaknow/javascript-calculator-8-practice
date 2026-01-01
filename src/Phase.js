c29ef8f5b4b8cb074b4da614b27cb08713670ad5
yooaknow <dbdkfma1228@naver.com>
2026-01-02 01:28:05 +0900

    , ; 더하기 로직 구현 완료

--- src/Phase.js
+++ src/Phase.js
@@ -3,10 +3,7 @@ import { Console } from "@woowacourse/mission-utils";
 export function Phase(trim_input){
 
   let answer =0;
-  
-  // 숫자 들어와지고 있음. 
-  // 이를 통해 . ;로 나눠지는 로직과, 
-  // // \n 안에 있는 것으로 나눠지게 하는 로직 구현 해야함 "
+
 
   if (trim_input.startsWith("//")){
     let start = trim_input.indexOf("//") +2;
@@ -26,11 +23,18 @@ export function Phase(trim_input){
     }
 
     Console.print(answer);
+    return answer;
 
   } else{
-    let arrayOfString = trim_input.split(/,:/);
+    let arrayOfString = trim_input.split(/[,;]/);
 
     Console.print(`, ;으로 나눠진 값들: ${arrayOfString}`);
+
+    for (let i =0; i <arrayOfString.length; i++) {  
+      answer += Number(arrayOfString[i]);  
+    }
+
+    return answer;
   }
 
 
